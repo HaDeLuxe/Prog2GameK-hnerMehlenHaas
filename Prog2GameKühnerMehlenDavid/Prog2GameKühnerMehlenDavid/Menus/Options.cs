9f420b6c5b5ccd386ac6b41d280ba5b1a89f67ea
Pascal Haas <haas_pa@hotmail.de>
2018-09-21 23:50:31 +0200

    Sprite added for MarCus

--- Prog2GameKühnerMehlenDavid/Prog2GameKühnerMehlenDavid/Menus/Options.cs
+++ Prog2GameKühnerMehlenDavid/Prog2GameKühnerMehlenDavid/Menus/Options.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +9,17 @@ using System.Threading.Tasks;
 namespace Reggie.Menus {
     class Options {
 
+        Slider slider = null;
+
+        public Options(Dictionary<string, Texture2D> texturesDictionary, Matrix transformationMatrix)
+        {
+            slider = new Slider(100);
+        }
+
+        //public void drawOptions()
+        //{
+        //    slider.drawSlider();
+        //}
         
     }
 }
